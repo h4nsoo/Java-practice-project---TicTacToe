21f39b6ba1b1797617ab88859687d1449c336d83
h4nsoo <mbhansoo05@gmail.com>
2025-03-23 02:15:07 +0100

    TicTacToe game finished - first project in Java

--- src/Main.java
+++ src/Main.java
@@ -11,31 +11,74 @@ public class Main {
 
         Boolean x = true;
 
-        while (!checkWinner()){
+        printBoard(board);
+
+        while (!checkWinner(board)){
             x = toggle(x, board, scanner);
             printBoard(board);
         }
 
+
+
+
     }
 
 
-
-
     static void printBoard(Character[] board){
+
         System.out.println(board[0] + " | " + board[1] + " | " + board[2]);
         System.out.println("---------");
         System.out.println(board[3] + " | " + board[4] + " | " + board[5]);
         System.out.println("---------");
         System.out.println(board[6] + " | " + board[7] + " | " + board[8]);
+
     }
 
-    static Boolean checkWinner(){
+    static boolean checkWinner(Character[] board) {
+        if (isWinner(board, 'x')) {
+            System.out.println("X Wins!");
+            return true;
+        }
+        if (isWinner(board, 'o')) {
+            System.out.println("O Wins!");
+            return true;
+        }
+        if (isDraw(board)) {
+            System.out.println("It's a Draw!");
+            return true;
+        }
         return false;
+    }
+
+    static Boolean isWinner(Character[] board, Character currentPlay) {
+
+        return ((board[0] == currentPlay && board[1] == currentPlay && board[2] == currentPlay) ||
+                (board[3] == currentPlay && board[4] == currentPlay && board[5] == currentPlay) ||
+                (board[6] == currentPlay && board[7] == currentPlay && board[8] == currentPlay) ||
+
+                (board[0] == currentPlay && board[3] == currentPlay && board[6] == currentPlay) ||
+                (board[1] == currentPlay && board[4] == currentPlay && board[7] == currentPlay) ||
+                (board[2] == currentPlay && board[5] == currentPlay && board[8] == currentPlay) ||
+
+                (board[0] == currentPlay && board[4] == currentPlay && board[8] == currentPlay) ||
+                (board[2] == currentPlay && board[4] == currentPlay && board[6] == currentPlay));
+
+
+        }
+
+    static boolean isDraw(Character[] board) {
+        for (Character cell : board) {
+            if (Character.isDigit(cell)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     static Boolean toggle(Boolean ok, Character[] board, Scanner scanner){
 
-        Character currentPlay;
+        char currentPlay;
+
         if (ok){
             System.out.print("It's X's turn, what's your move : ");
             currentPlay = 'x';
@@ -46,7 +89,7 @@ public class Main {
         }
 
 
-        Integer selectedCell = scanner.nextInt();
+        int selectedCell = scanner.nextInt();
 
         while (!checkPlay(board, selectedCell)){
             System.out.println("Invalid move, Try again !");
@@ -63,12 +106,8 @@ public class Main {
 
     static Boolean checkPlay(Character[] board, Integer selectedCell){
         Character n = board[selectedCell];
-        if (n == 'x' || n == 'o'){
-            return false;
-        }
-        else{
-            return true;
-        }
+        return (Character.isDigit(n));
+
     }
 
 
